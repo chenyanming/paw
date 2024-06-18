a3661b6a50ec4400d61e063a5a66cd24aa47bd28
chenyanming <elecming@gmail.com>
2024-06-18 16:07:58 +0800

    paw-android: support moji, chatgpt, share menu and Tasker.

--- paw-android.el
+++ paw-android.el
@@ -5,6 +5,17 @@
   :type 'string
   :group 'paw)
 
+
+(defcustom paw-moji-android-program "com.mojitec.mojidict"
+  "The Moji android program."
+  :type 'string
+  :group 'paw)
+
+(defcustom paw-chatgpt-android-program "com.openai.chatgpt"
+  "The chatgpt android program."
+  :type 'string
+  :group 'paw)
+
 (defun paw-eudic-search-details (&optional word)
   "Call `paw-eudic-android-program' in termux to search for WORD."
   (interactive)
@@ -16,12 +27,60 @@
                  (t (current-word t t)))
            paw-eudic-android-program)))
 
+
+
+(defun paw-moji-search-details (&optional word)
+  "Call `paw-moji-android-program' in termux to search for WORD."
+  (interactive)
+  (call-process-shell-command
+   (format "termux-am start -a android.intent.action.SEND --es android.intent.extra.TEXT \"%s\" -t text/* %s"
+           (cond ((stringp word) word)
+                 ((use-region-p)
+                  (replace-regexp-in-string "[ \n]+" " " (replace-regexp-in-string "^[ \n]+" "" (buffer-substring-no-properties (region-beginning) (region-end)))))
+                 (t (current-word t t)))
+           paw-moji-android-program)))
+
+(defun paw-android-search-details (&optional word)
+  "Call share menu in termux to search for WORD."
+  (interactive)
+  (call-process-shell-command
+   (format "termux-am start -a android.intent.action.SEND --es android.intent.extra.TEXT \"%s\" -t text/*"
+           (cond ((stringp word) word)
+                 ((use-region-p)
+                  (replace-regexp-in-string "[ \n]+" " " (replace-regexp-in-string "^[ \n]+" "" (buffer-substring-no-properties (region-beginning) (region-end)))))
+                 (t (current-word t t)))
+           )))
+
+(defun paw-chatgpt-search-details (&optional word)
+  "Call `paw-chatgpt-android-program' in termux to search for WORD."
+  (interactive)
+  (call-process-shell-command
+   (format "termux-am start -a android.intent.action.SEND --es android.intent.extra.TEXT \"%s\" -t text/plain %s"
+           (cond ((stringp word) word)
+                 ((use-region-p)
+                  (replace-regexp-in-string "[ \n]+" " " (replace-regexp-in-string "^[ \n]+" "" (buffer-substring-no-properties (region-beginning) (region-end)))))
+                 (t (current-word t t)))
+           paw-chatgpt-android-program)))
+
 (defun paw-android-browse-url (url)
   "Open given URL in Termux."
   (interactive "sEnter the URL: ")
   (if (string-match-p "^http\\(s\\)?://[^ \n]*$" url)
       (call-process-shell-command (format "termux-open %s" url))
     (message "Invalid URL. Please enter a URL that begins with http://")))
+
+(defun paw-android-language-convert(lang)
+  "TODO Convert the detected langauge to android google tts recognized language."
+  (pcase lang
+    ("ja" "jpn-jpn")
+    ("en" "eng-usa")
+    (_ lang)))
+
+(defun paw-android-say-word (word &optional lang)
+  "Say word with Tasker"
+  (call-process-shell-command (format "am broadcast --user 0 -a net.dinglish.tasker.emacs -e word \"%s\" -e lang \"%s\"" word (paw-android-language-convert (if lang lang (paw-check-language word))))))
+
+
 
 
 (provide 'paw-android)

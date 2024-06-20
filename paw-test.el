6bbc2a654613a31baec3ee4d23af4e6329e74d76
chenyanming <elecming@gmail.com>
2024-06-20 22:42:27 +0800

    Add paw-test-language

--- paw-test.el
+++ paw-test.el
@@ -52,3 +52,20 @@
 (benchmark 1 '(paw-annotation-mode 1))
 
 (benchmark 1 '(paw-view-note))
+
+"¿Qué hora es?"
+
+"Buenos días."
+
+
+"Lontemps je me suis couché de bonne heure.Parfois,à peine ma bougie éteinte, mes yeux se fermaient si vite que je je n’avait pas le temps de me dire:“je m’endors”Et, une demi-heure après,la pensée qu’il étaits temps de chercher le sommeil m’éveillait."
+
+"Die „Albertina“ ist die Bibliothek der Universität Leipzig. Das Haus in der Beethoven-straße 6 ist alt, aber die Bibliothek ist sehr modern. Viele Studentinnen und Studenten arbeiten in den Lesesälen in der ersten Etage."
+
+
+(defun paw-test-language()
+  (assert (string= (paw-check-language "A wiki (/ˈwɪki/ ⓘ WI-kee) is a form of online hypertext publication that is collaboratively edited and managed by its own audience directly through a web browser.") "en") t "Wrong Langauge")
+  (assert (string= (paw-check-language "대한의사협회(의협)의 집단 휴진에 불참하는 의사들이 속속 나오고 있다. 분만병의원협회, 대한아동병원협회에 이어 각 대학병원 뇌전증 전문 교수들이 모인 거점 뇌전증지원병원 협의체 역시 18일로 예정된 의협의 집단 휴진에 불  참한다.") "ko") t "Wrong Langauge")
+  (assert (string= (paw-check-language "「不論人生或商場，各種情形都需要蒐集對的情報、進行建設性的思考，並做出正確決斷。本書提供一個從思考到決策的完整框架，可以幫助讀者避免受到常見盲點的影響，客觀做出正確的決定，提高人生和商場上各種試煉的打擊率，非常值得一讀。」") "zh") t "Wrong Langauge")
+  (assert (string= (paw-check-language "距盐城湖一小时车程外，是犹他州一个名为伊甸的小镇，小镇上有一座名叫Powder的山。山上绿树成荫，白雪皑皑。2013年，一群20多岁的有志青年怀揣着400万美元来到这里，买下了1万英亩的土地。他们打算把这里建成一个生态住宅区，以吸引一些成功的企业家。") "zh") t "Wrong Langauge")
+  )

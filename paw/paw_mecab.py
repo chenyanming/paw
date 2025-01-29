b1b286f0ea8e07fa4edc7693772fcb1d8ef31a00
chenyanming <elecming@gmail.com>
2025-01-29 23:10:09 +0900

    Support janome as well

--- paw/paw_mecab.py
+++ paw/paw_mecab.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 mecab_imported = False
 tokenizer = None
-# def segmentation(text):
-#     try:
-#         import MeCab
-#         tokenizer = MeCab.Tagger("-Owakati")
-#         mecab_imported = True
-#     except ImportError:
-#         from janome.tokenizer import Tokenizer
-#         tokenizer = Tokenizer()
-#     if tokenizer:
-#         if mecab_imported:
-#             tokens = tokenizer.parse(text)
-#         else:
-#             tokens = tokenizer.tokenize(text)
-#     return tokens
-
 def segmentation(text):
-    import MeCab
-    tokenizer = MeCab.Tagger("-Owakati")
-    tokens = tokenizer.parse(text)
+    try:
+        import MeCab
+        tokenizer = MeCab.Tagger("-Owakati")
+        mecab_imported = True
+    except ImportError:
+        from janome.tokenizer import Tokenizer
+        tokenizer = Tokenizer()
+    if tokenizer:
+        if mecab_imported:
+            tokens = tokenizer.parse(text)
+        else:
+            tokens = tokenizer.tokenize(text)
     return tokens
+
+# def segmentation(text):
+#     import MeCab
+#     tokenizer = MeCab.Tagger("-Owakati")
+#     tokens = tokenizer.parse(text)
+#     return tokens

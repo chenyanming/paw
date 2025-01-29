32897f6d86ed83e6c3048f5b1e35805f52293ac3
chenyanming <elecming@gmail.com>
2025-01-29 21:26:23 +0900

    Fix paw-server can load load issue

--- paw-server.el
+++ paw-server.el
@@ -24,38 +24,33 @@
         (progn
           (message "Starting paw-server with wallabag features...")
           (require 'wallabag)
-          (start-process "paw-server"
-                         "*paw-server*"
-                         paw-python-program
-                         paw-server-program
-                         "run_server"
-                         "--database"
-                         paw-db-file
-                         "--save-dir"
-                         temporary-file-directory
-                         "--port"
-                         paw-server-port
-                         "--wallabag-host"
-                         wallabag-host
-                         "--wallabag-username"
-                         wallabag-username
-                         "--wallabag-password"
-                         wallabag-password
-                         "--wallabag-clientid"
-                         wallabag-clientid
-                         "--wallabag-secret"
-                         wallabag-secret))
+          (let ((cmd (if (executable-find "paw")
+                         (list paw-server-program)
+                       (list paw-python-program paw-server-program))))
+            (apply #'start-process
+                   "paw-server"
+                   "*paw-server*"
+                   (append cmd
+                           (list "run_server"
+                                 "--database" paw-db-file
+                                 "--save-dir" temporary-file-directory
+                                 "--port" paw-server-port
+                                 "--wallabag-host" wallabag-host
+                                 "--wallabag-username" wallabag-username
+                                 "--wallabag-password" wallabag-password
+                                 "--wallabag-clientid" wallabag-clientid
+                                 "--wallabag-secret" wallabag-secret)))))
       (message "Starting paw-server...")
-      (start-process "paw-server"
-                     "*paw-server*"
-                     paw-python-program
-                     paw-server-program
-                     "run_server"
-                     "--database"
-                     paw-db-file
-                     "--save-dir"
-                     temporary-file-directory
-                     "--port"
-                     paw-server-port))))
+      (let ((cmd (if (executable-find "paw")
+                     (list paw-server-program)
+                   (list paw-python-program paw-server-program))))
+        (apply #'start-process
+               "paw-server"
+               "*paw-server*"
+               (append cmd
+                       (list "run_server"
+                             "--database" paw-db-file
+                             "--save-dir" temporary-file-directory
+                             "--port" paw-server-port)))))))
 
 (provide 'paw-server)

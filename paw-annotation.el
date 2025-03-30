b92c6e94fa4c0a1f63cc0db940af9d85199e436a
chenyanming <elecming@gmail.com>
2025-03-30 23:38:32 +0900

    Add text-mode and markdown-mode as default

--- paw-annotation.el
+++ paw-annotation.el
@@ -21,7 +21,17 @@
 (declare-function evil-define-key* "ext:evil-core.el" t t)
 
 (defcustom paw-annotation-mode-supported-modes
-  '(nov-mode org-mode paw-view-note-mode wallabag-entry-mode eww-mode eaf-mode elfeed-show-mode pdf-view-mode telega-webpage-mode)
+  '(nov-mode
+    org-mode
+    paw-view-note-mode
+    wallabag-entry-mode
+    eww-mode
+    eaf-mode
+    elfeed-show-mode
+    pdf-view-mode
+    telega-webpage-mode
+    text-mode
+    markdown-mode)
   "Supported modes for paw-annotation-mode."
   :group 'paw
   :type 'list)

e00ae68b58fd95d52358aab24a29b26691eebc9e
chenyanming <elecming@gmail.com>
2025-04-05 10:52:13 +0900

    Update paw-mac-dictionary-program

--- paw-mac.el
+++ paw-mac.el
@@ -3,7 +3,10 @@
 ;;; paw-mac.el -*- lexical-binding: t; -*-
 
 (defcustom paw-mac-dictionary-program "mac"
-  "Executable used to access the mac."
+  "Executable used to access the mac.
+mac: the Mac dictionary.
+eudic: the eudic dictionary.
+"
   :type 'file)
 
 ;;;###autoload
@@ -11,8 +14,11 @@
   "Search word with mac."
   (interactive)
   (let ((word (or word (paw-get-word))))
-    (shell-command-to-string
-     (format "osascript -e 'tell application \"Eudic\" to activate show dic with word \"%s\"'" word))))
+    (if (string= paw-mac-dictionary-program "mac")
+        (shell-command-to-string
+         (format "open dict://%s" (url-hexify-string word)))
+      (shell-command-to-string
+       (format "osascript -e 'tell application \"Eudic\" to activate show dic with word \"%s\"'" word)))))
 
 
 (provide 'paw-mac)

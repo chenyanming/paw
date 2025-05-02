d851670da63cc03ca86613c5457f5b2c1391208b
Damon Chan <chenyanming@users.noreply.github.com>
2025-05-02 11:09:55 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip install emacs-paw
+pip install edge-tts
+pip install lingua-language-detector
+python -m nltk.downloader stopwords
+python -m nltk.downloader punkt
+python -m nltk.downloader punkt_tab

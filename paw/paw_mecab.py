2a14b8952c4dccf7e4770aef95119aa15d9812c0
chenyanming <elecming@gmail.com>
2025-07-28 23:31:17 +0900

    Update paw_mecab

--- paw/paw_mecab.py
+++ paw/paw_mecab.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
-mecab_imported = False
-tokenizer = None
-def segmentation(text):
-    global mecab_imported, tokenizer
-    try:
-        import MeCab
-        tokenizer = MeCab.Tagger("-Owakati")
-        mecab_imported = True
-    except:
-        from janome.tokenizer import Tokenizer
-        tokenizer = Tokenizer()
-    if tokenizer:
-        if mecab_imported:
-            words = tokenizer.parse(text)
-        else:
-            tokens = tokenizer.tokenize(text)
-            words = " ".join(token.surface for token in tokenizer.tokenize(text))
-    return words
 
-# def segmentation(text):
-#     import MeCab
-#     tokenizer = MeCab.Tagger("-Owakati")
-#     tokens = tokenizer.parse(text)
-#     return tokens
+from janome.tokenizer import Tokenizer
+
+# only use janome to do segmentation, show the base form
+def segmentation(text):
+    tokenizer = Tokenizer()
+    words = [token.base_form if token.base_form != '*' else token.surface for token in tokenizer.tokenize(text)]
+    return " ".join(words)
